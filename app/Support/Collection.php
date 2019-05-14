b94b1477f1fcfe5fc043d1e6d2a07bbe4ec90178
Ajayi Nurudeen <ajayinurudeen998@gmail.com>
2019-05-14 13:54:10 +0100

    checking if collection add to existing

--- app/Support/Collection.php
+++ app/Support/Collection.php
@@ -2,13 +2,50 @@
 
 namespace App\Support;
 
-class Collection {
+use ArrayIterator;
+
+class Collection implements \IteratorAggregate {
 
 	protected $items = [];
+
+	//instantiate constructors parameter to empty because any other function could call this class
+	//without passing any parameter.. just to avoid erros/bugs
+	public function __construct(array $items = [])
+	{
+		//set the array to the items property
+		$this->items = $items;
+	}
+
+	//returns all ittems passed into this class whenever instantiated by another class/function
 
 	public function get()
 	{
 		return $this->items;
+	}
+
+	public function count()
+	{
+		return count($this->items);
+	}
+
+
+	//our interface requires this function to work
+	public function getIterator()
+	{
+		return new ArrayIterator($this->items);
+	}
+
+	public function add(array $items)
+	{
+		//adding two arrays together..
+		$this->items = array_merge($this->items, $items);
+	}
+
+	//pass collection parameter because we always expect a collection to be passed in to this function
+	public function merge(Collection $collection)
+	{
+		// return $this->add($collection->get());
+		return new Collection(array_merge($this->get(), $collection->get()));
 	}
 }
  ?>
--- tests/unit/CollectionTest.php
+++ tests/unit/CollectionTest.php
@@ -1,4 +1,5 @@
 <?php 
+
 	//this test class is based on collection class we wana run a test on
 	class CollectionTest extends \PHPUnit\Framework\TestCase
 	{
@@ -8,6 +9,86 @@
 			$collection = new \App\Support\Collection;
 
 			$this->assertEmpty($collection->get());
+		}
+
+		/** @test */
+		public function countIsCorrectForItemsPassedIn()
+		{
+			$collection = new \App\Support\Collection([
+				'one', 'two', 'three'
+			]);
+
+			//first parameter is always what we are expecting..
+			$this->assertEquals(3, $collection->count());
+		}
+
+		//indirectly, below function is used to test if a model or a collection has these values set in it
+		/** @test */
+		public function items_returned_match_items_passed_in()
+		{
+			//could be a way of testing a model
+			$collection = new \App\Support\Collection([
+				'one', 'two', 'three'
+			]);
+
+			$this->assertCount(3, $collection->get());
+
+			$this->assertEquals($collection->get()[0], 'one');
+
+			$this->assertEquals($collection->get()[1], 'two');
+		}
+
+		/** @test */
+		public function collection_is_instance_of_iterator_aggregate()
+		{
+			$collection = new \App\Support\Collection();
+
+			$this->assertInstanceOf(IteratorAggregate::class, $collection);
+		}
+
+		/** @test */
+		public function collection_can_be_iterated()
+		{
+			$collection = new \App\Support\Collection([
+				'one', 'two', 'three'
+			]);
+
+			$items = [];
+
+			foreach ($collection as $item) {
+				$items[] = $item;
+			}
+
+			$this->assertCount(3, $items);
+
+			//specifically checking if this is an array iterator
+			//make sure it sends an array iterator
+			$this->assertInstanceOf(ArrayIterator::class, $collection->getIterator());
+		}
+
+		/** @test*/
+
+		public function checksIfTwoCollectionsContainsTheExpentedLengthOfParameters()
+		{
+			$collection1 = new \App\Support\Collection(['one', 'two', 'three']);
+
+			$collection2 = new \App\Support\Collection(['four', 'five', 'six']);
+
+			//merge both collection together
+			$newCollection = $collection1->merge($collection2);
+
+			//finally check if the merged collection length is upto 6
+			$this->assertEquals(6, $newCollection->count());
+		}
+
+
+		/** @test */
+		public function canAddToExistingCollection()
+		{
+			$collection = new \App\Support\Collection(['one', 'two']);
+			$collection->add(['three']);
+
+			$this->assertCount(3, $collection->get());
 		}
 	}
  ?>
